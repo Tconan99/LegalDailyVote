42dac71e45f838f6d24d00e48cf7c2d652445e1b
Tconan99 <627594232@qq.com>
2018-06-27 06:38:08 +0800

    添加header信息，仿照合理的投票方式

--- legaldaily.py
+++ legaldaily.py
@@ -10,15 +10,28 @@ def vote():
         'w_id': 45
     }
 
-    response = requests.post(url, data=params)
+    headers = {
+        'Accept': '*/*',
+        'Accept-Encoding':'gzip, deflate',
+        'Accept-Language':'zh-CN,zh;q=0.9,en;q=0.8,ru;q=0.7',
+        'Connection':'Keep-Alive',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'Host': 'zbz.legaldaily.com.cn',
+        'Origin':'http://zbz.legaldaily.com.cn',
+        'Referer':'http://zbz.legaldaily.com.cn/works/index.php',
+        'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36',
+        'X-Requested-With': 'XMLHttpRequest'
+    }
+
+    response = requests.post(url, data=params, headers=headers)
     global number
-    print(number, "->", response.text)
+    print(number, "->", eval("u'" + response.text + "'"))
     number = number + 1
 
 def loop():
     vote()
     global timer
-    timer = threading.Timer(0.05, loop)
+    timer = threading.Timer(5, loop)
     timer.start()
 
 timer = threading.Timer(0.1, loop)
